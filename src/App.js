27820b6403dec22ba921e02c37b197c4524320cf
avinasdube <avidubey712@gmail.com>
2026-05-13 15:19:30 +0530

    refactor: standardize import statements and improve loading screen logic

--- src/App.js
+++ src/App.js
@@ -1,46 +1,48 @@
 // IMPORTING NECESSARY DEPENDENCIES
 
-import './style.scss';
+import "./style.scss";
 
-import LoadingScreen from './components/LoadingScreen/LoadingScreen';
-import NavigationSection from './components/NavigationSection/NavigationSection';
+import LoadingScreen from "./components/LoadingScreen/LoadingScreen";
+import NavigationSection from "./components/NavigationSection/NavigationSection";
 
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 
-import { useEffect, useState } from 'react';
-import AboutMe from './components/About/AboutMe';
-import Layout from './layout/Layout';
-import FooterSection from './components/FooterSection/FooterSection';
+import { useEffect, useState } from "react";
+import AboutMe from "./components/About/AboutMe";
+import FooterSection from "./components/FooterSection/FooterSection";
+import Layout from "./layout/Layout";
 
 function App() {
-
-  // CODE TO IMPLEMENT LOADING SCREEN 
-
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 1600);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, []);
 
   return (
     <>
-      {loading ? <LoadingScreen /> :
-
-        (<div className="app">
+      {loading ? (
+        <LoadingScreen />
+      ) : (
+        <div className="app">
           <Router>
             <NavigationSection />
             <Routes>
-              <Route exact path="/" element={<Layout />} />
+              <Route path="/" element={<Layout />} />
               <Route path="/about" element={<AboutMe />} />
             </Routes>
             <FooterSection />
           </Router>
-        </div>)}
+        </div>
+      )}
     </>
-  )
+  );
 }
 
 export default App;
